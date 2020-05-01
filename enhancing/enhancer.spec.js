ed1b4cd25948f305a135c6414089a457ecfeaf04
Roskideluge <roberto.delgado@gmail.com>
2020-04-30 17:15:47 -1000

    succeed() passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -6,7 +6,19 @@ describe('enhancing system', () => {
             const item = {
                 durability: 0
             }
-            expect(repair(item)).toEqual({durability: 100})
-        })
+            const item2 = {
+                durability: 10
+            }
+            expect(repair(item).durability).toBe(100)
+            expect(repair(item2).durability).toBe(100)       
+        });
+        it('increases enhancement by 1 up to 20, durability is unchanged', () => {
+            const item = {
+                durability: 50,
+                enhancement: 15
+            }
+            expect(succeed(item).enhancement).toBe(16)
+        });
+        it.todo('decreases durability by 5 if enhancement < 15; by 10 if enhancement is > 15')
     })
 })
