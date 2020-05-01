704058155c411a31796c8757ab6e4091a8f3ca11
Roskideluge <roberto.delgado@gmail.com>
2020-04-30 16:30:43 -1000

    repair() test passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,12 @@
-const enhancer = require('./enhancer.js');
+const { succeed, repair, fail, get } = require('./enhancer.js');
 // test away!
+describe('enhancing system', () => {
+    describe('repair()', () => {
+        it('should restore durability to 100', () => {
+            const item = {
+                durability: 0
+            }
+            expect(repair(item)).toEqual({durability: 100})
+        })
+    })
+})
